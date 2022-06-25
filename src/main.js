0aa7c2a5d91d68f685f871c5917f909891ab934b
LE123123 <heart20021010@gmail.com>
2022-06-25 15:26:18 +0900

    asdf

--- src/main.js
+++ src/main.js
@@ -53,13 +53,13 @@ const app = new Koa();
 const router = new Router();
 
 // TODO: 다른 도메인에서의 cookie공유 (x)
-let corsOption = {
-  origin: process.env.CLIENT_HOST,
-  credentials: true,
-};
+// let corsOption = {
+//   origin: process.env.CLIENT_HOST,
+//   credentials: true,
+// };
 
-app.proxy = true;
-app.use(cors(corsOption));
+// app.proxy = true;
+// app.use(cors(corsOption));
 app.use(serve(__dirname + '/uploads'));
 
 router.use('/api', api.routes());
@@ -69,7 +69,7 @@ app.use(jwtMiddleware);
 
 router.post('/api/upload', upload.single('image'), (ctx) => {
   console.log(ctx.file);
-  const IMG_URL = `${process.env.CLIENT_HOST}/${ctx.file.filename}`;
+  const IMG_URL = `${process.env.SERVER_HOST}/${ctx.file.filename}`;
   console.log(IMG_URL);
   ctx.body = { url: IMG_URL };
 });
